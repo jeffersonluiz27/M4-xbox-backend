7928293d5585df3c861bbda3d660efcac8072bb7
jeffersonluiz27 <developer.jefferson27@outlook.com>
2022-05-21 13:30:53 -0300

    FindAll and prisma construtor in game

--- src/game/game.service.ts
+++ src/game/game.service.ts
@@ -1,17 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { CreateGameDto } from './dto/create-game.dto';
 import { Game } from './entities/game.entity';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class GameService {
   games: Game[] = [];
 
-  create(createGameDto: CreateGameDto): Game {
-    const game: Game = {
-      id: 'id_aleatorio',
-      ...createGameDto,
-      title: undefined,
-    };
+  constructor(private readonly prisma: PrismaService) {}
+
+  findAll() {
+    return this.prisma.game.findMany();
+  }
+
+  create(dto: CreateGameDto): Game {
+    const game: Game = { id: 'id_aleatorio', ...dto, title: undefined };
     this.games.push(game);
     return game;
   }
