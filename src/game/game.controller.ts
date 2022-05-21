248801226f5c0c44eee85b42d888197a884ced66
jeffersonluiz27 <developer.jefferson27@outlook.com>
2022-05-21 13:31:03 -0300

    FindAll and prisma construtor in game

--- src/game/game.controller.ts
+++ src/game/game.controller.ts
@@ -2,12 +2,20 @@ import { Body, Controller, Get, Post } from '@nestjs/common';
 import { GameService } from './game.service';
 import { Game } from './entities/game.entity';
 import { CreateGameDto } from './dto/create-game.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Games')
 @Controller('Games')
 export class GameController {
   constructor(private gameService: GameService) {}
+
+  @Get()
+  @ApiOperation({
+    summary: 'Listar todos os jogos',
+  })
+  findAll() {
+    return this.gameService.findAll();
+  }
 
   @Post()
   create(@Body() createGameDto: CreateGameDto): Game {
