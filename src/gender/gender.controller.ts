a15192284baa5ffef91b39b30eb9d19587608c8a
jeffersonluiz27 <developer.jefferson27@outlook.com>
2022-05-21 13:31:12 -0300

    FindAll and prisma construtor in gender

--- src/gender/gender.controller.ts
+++ src/gender/gender.controller.ts
@@ -2,12 +2,20 @@ import { Body, Controller, Get, Post } from '@nestjs/common';
 import { GenderService } from './gender.service';
 import { Gender } from './entities/gender.entity';
 import { CreateGenderDto } from './dto/create-gender.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Genders')
 @Controller('Genders')
 export class GenderController {
   constructor(private genderService: GenderService) {}
+
+  @Get()
+  @ApiOperation({
+    summary: 'Listar todos os generos',
+  })
+  findAll() {
+    return this.genderService.findAll();
+  }
 
   @Post()
   create(@Body() createGenderDto: CreateGenderDto): Gender {
--- src/gender/gender.service.ts
+++ src/gender/gender.service.ts
@@ -1,17 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { CreateGenderDto } from './dto/create-gender.dto';
 import { Gender } from './entities/gender.entity';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class GenderService {
   genders: Gender[] = [];
 
-  create(createGenderDto: CreateGenderDto): Gender {
-    const gender: Gender = {
-      id: 'id_aleatorio',
-      ...createGenderDto,
-      name: undefined,
-    };
+  constructor(private readonly prisma: PrismaService) {}
+
+  findAll() {
+    return this.prisma.gender.findMany();
+  }
+
+  create(dto: CreateGenderDto): Gender {
+    const gender: Gender = { id: 'id_aleatorio', ...dto, name: undefined };
     this.genders.push(gender);
     return gender;
   }
