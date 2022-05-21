83ca9f266fa0a7a6a4735f8a2eba9390558575a8
jeffersonluiz27 <developer.jefferson27@outlook.com>
2022-05-21 13:04:42 -0300

    rebuild names colluns entity games

--- src/game/entities/game.entity.ts
+++ src/game/entities/game.entity.ts
@@ -1,4 +1,10 @@
 export class Game {
-  id: string;
-  Nome: String;
+  id?: string;
+  title: String;
+  coverImageUrl?: String;
+  description?: String;
+  year?: Date;
+  imdbScore?: Number;
+  trailerYouTubeUrl?: String;
+  gameplayYouTubeUrl?: String;
 }
